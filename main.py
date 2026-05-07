d5441b55ee6f802b41599a5455f7cfb1f0e9071e
ydborse <ydborse370823@kkwagh.edu.in>
2026-05-07 06:51:45 +0530

    yeppp

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from flask import Flask
-
-app = Flask(__name__)
+from flask import Flask 
+app = flask (__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World from Google Engine!"
+    return "hello world"
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080, debug=True)
+app.run("host=127.0.0.1", port=8080, debug=True)
